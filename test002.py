045929b3f0d7ac851b23a72108083fe29f1c418a
laoxue0608 <93564958+laoxue0608@users.noreply.github.com>
2023-08-30 18:55:27 +0800

    fix09  123123

--- test002.py
+++ test002.py
@@ -2,6 +2,9 @@ if __name__ == '__main__':
     print('02')
     print('test----------')
     print('test2----------')
-    print('test5----------')
-    print('test2----------')
+    print('test4----------')
+    print('test1----------')
+    print('test3----------')
 
+
+    print('test3----------')
