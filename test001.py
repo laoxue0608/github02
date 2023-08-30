c350f90140030540b965f9ad4ca0c8cf2122abcd
lixiaobo <xiaobo.li@cootek.cn>
2023-08-30 14:54:27 +0800

    fix07 修改test007

--- test001.py
+++ test001.py
@@ -2,10 +2,7 @@ if __name__ == '__main__':
     print(1)
     print('003')
     print('004')
+
     print('005')
     print('005')
-    print('005')
-    print('005')
-    print('005')
-    print('005')
-    print('005')
+    print('007')
